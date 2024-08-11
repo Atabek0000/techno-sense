5dd4c8b5bfafba2e718f349d3434e63447b1feea
King YouTube <167699038+Atabek0000@users.noreply.github.com>
2024-08-11 19:52:57 +0600

    Add files via upload

---
+++ BOOK/static/js/ about.js
@@ -0,0 +1,22 @@
+function goBack() {
+    window.history.back();
+}
+
+function deleteBook(title) {
+    if (confirm("Are you sure you want to delete this book?")) {
+        fetch(`/delete_book`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: title })
+        })
+        .then(response => {
+            if (response.ok) {
+                location.reload();
+            } else {
+                alert("Failed to delete the book.");
+            }
+        });
+    }
+}
